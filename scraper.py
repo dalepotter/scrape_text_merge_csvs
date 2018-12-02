b7707fdbcfeb63036f8481e50dd61d92f98a3d30
Dale Potter <dalepotter@gmail.com>
2018-12-02 11:59:47 +0000

    Define pages and make request

--- scraper.py
+++ scraper.py
@@ -1,7 +1,22 @@
+# Import required libraries
+import requests  # Makes and returns HTTP requests
+
+# Define a list of pages
+pages = [
+    "https://www.ethnicity-facts-figures.service.gov.uk/british-population/demographics/male-and-female-populations/latest",
+    "https://www.ethnicity-facts-figures.service.gov.uk/british-population/demographics/working-age-population/latest"
+    ]
+
 # For a list of given webpages
-    # Make request to the page
+for page in pages:
+    # Each of our defined pages is assigned the variable 'page'
+
     # Make HTTP request to get the page content
+    req = requests.get(page)
+
     # Access the text (possibly specific items) - this is 'output1'
+
     # Access CSV data (own file) - this is 'output2'
+
 
 # Merge all outputs into an XLS
