51a36f265ad06af6294a2b314101e6fda994ce2a
Dale Potter <dalepotter@gmail.com>
2018-12-02 11:39:52 +0000

    Add pseudocode for scraper

---
+++ scraper.py
@@ -0,0 +1,7 @@
+# For a list of given webpages
+    # Make request to the page
+    # Make HTTP request to get the page content
+    # Access the text (possibly specific items) - this is 'output1'
+    # Access CSV data (own file) - this is 'output2'
+
+# Merge all outputs into an XLS
